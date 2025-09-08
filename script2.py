4498594c49f7b6f186d5fdf7eebe6d4eb9305b88
Matthew Hile <mattwhile83@gmail.com>
2025-09-08 18:02:00 -0400

    Uploading tsv files from local dir to Azure blob storage working

--- script2.py
+++ script2.py
@@ -16,11 +16,14 @@ directory = r"C:\Users\MatthewHile\Desktop\Labs\imdb_to_azure\imdb_tsv_files"
 
 # Pass in the connection strings from .env
 sql_conn_sting = os.getenv("AZURE_CONN_STRING")
-
 storage_conn_string = os.getenv("AZURE_STORAGE_CONN_STRING")
+
+container_name = "stagetsv"
 
 # Create the BlobServiceClient
 blob_service_client = BlobServiceClient.from_connection_string(storage_conn_string)
+container_client = blob_service_client.get_container_client(container_name)
+#container_client.create_container(exist_ok=True) # Ensures the blob container exists (creates it if missing).
 
 # Create SQLAlchemy engine
 engine = sa.create_engine(sql_conn_sting)
@@ -28,6 +31,15 @@ engine = sa.create_engine(sql_conn_sting)
 with engine.connect() as connection:
     print("Connected to Azure SQL Database")
 
+for file in os.listdir(directory):
+    if file.endswith(".tsv"):
+        file_path = os.path.join(directory, file) # read tsv
+
+        blob_client = container_client.get_blob_client(file)
+        
+        with open(file_path, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+        print(f"\nUploaded {file} to Azure Blob Storage")
 
 
 connection.close()
--- test_connection.py
+++ test_connection.py
@@ -1,6 +1,7 @@
 import pyodbc, os
 import sqlalchemy as sa
 from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
 
 load_dotenv()
 
@@ -16,10 +17,24 @@ load_dotenv()
 
 connection_string = os.getenv("AZURE_CONN_STRING")
 
+storage_conn_string = os.getenv("AZURE_STORAGE_CONN_STRING")
+
+# Create the BlobServiceClient
+blob_service_client = BlobServiceClient.from_connection_string(storage_conn_string)
+
 # Create SQLAlchemy engine
 engine = sa.create_engine(connection_string)
 
 with engine.connect() as connection:
     print("Connected to Azure SQL Database!")
+
+print("\nTesting connection to Azure Blob Storae:")
+try:
+    containers = blob_service_client.list_containers()
+    for c in containers:
+        print(f"Container found: {c['name']}")
+    print("Successfully connected to Blob Storage")
+except Exception as e:
+    print("Failed to connect to Blob Storage:", e)
 
 connection.close()
