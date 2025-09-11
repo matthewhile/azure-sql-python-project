859c01203adaa9fa1c232a36fc989c9ae90769b8
Matthew Hile <mattwhile83@gmail.com>
2025-09-10 20:22:40 -0400

    Added logic to create staging table for each tsv file uploaded to blob storage

--- script2.py
+++ script2.py
@@ -23,25 +23,43 @@ container_name = "stagetsv"
 # Create the BlobServiceClient
 blob_service_client = BlobServiceClient.from_connection_string(storage_conn_string)
 container_client = blob_service_client.get_container_client(container_name)
-#container_client.create_container(exist_ok=True) # Ensures the blob container exists (creates it if missing).
 
 # Create SQLAlchemy engine
 engine = sa.create_engine(sql_conn_sting)
 
-with engine.connect() as connection:
+with engine.begin() as connection:
     print("Connected to Azure SQL Database")
 
-for file in os.listdir(directory):
-    if file.endswith(".tsv"):
-        file_path = os.path.join(directory, file) # read tsv
+    for file in os.listdir(directory):
+        if file.endswith(".tsv"):
+            file_path = os.path.join(directory, file) # read tsv
 
-        blob_client = container_client.get_blob_client(file)
-        
-        with open(file_path, "rb") as data:
-            blob_client.upload_blob(data, overwrite=True)
-        print(f"\nUploaded {file} to Azure Blob Storage")
+            blob_client = container_client.get_blob_client(file) # create the blob object
+            
+            with open(file_path, "rb") as data:
+                blob_client.upload_blob(data, overwrite=True) # upload .tsv file and overwrite
+            print(f"\nUploaded {file} to Azure Blob Storage")
 
+            # Get staging table name
+            base_name = os.path.splitext(file)[0].replace(".", "_")
+            stg_table_name = f"stg_{base_name}"
+
+            blob_url = f"https://stimdbeastusdev002.blob.core.windows.net/{container_name}/{file}"
+
+            connection.execute(sa.text(f"""
+            IF OBJECT_ID('dbo.{stg_table_name}', 'U') IS NOT NULL
+                DROP TABLE dbo.{stg_table_name};
+            """))
+
+            # Create staging table for each file
+            connection.execute(sa.text(f"""
+            CREATE TABLE dbo.{stg_table_name} (
+                line NVARCHAR(MAX)
+            );
+            """))
+            print(f"Created table {stg_table_name}")
 
 connection.close()
 
-#print("\n All files processed successfully.")
+print("\nAll files processed successfully.")
+print(f"Script completed at {datetime.now()}")
